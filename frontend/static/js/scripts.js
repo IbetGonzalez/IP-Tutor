48cf3fa7aba75af474637bbfb722c82f9700cde9
Eduard Gonzalez <6415127+AncientHeroX@users.noreply.github.com>
2024-06-22 14:00:03 -0400

    initial footer complete
    basically done with landing layout.

--- frontend/static/js/scripts.js
+++ frontend/static/js/scripts.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
-    //document.getElementById("dropdownBtn").addEventListener("click", toggleDropDown);
     updateNav();
+    
 });
 document.addEventListener("htmx:afterRequest", updateNav);
 
@@ -14,27 +14,6 @@ function updateNav() {
     for (let i = 0; i < currSelNav.length; i++) {
         currSelNav.item(i).classList.remove("nav-selected");
     }
-    console.log(selNav);
     selNav.classList.add("nav-selected");
 
-}
-
-var toggleDropDown = debounce(() => {
-    let dropdownMenu = document.getElementById("dropdownMenu");  
-    let dropdownBtn = document.getElementById("dropdownBtn")
-
-    dropdownMenu.classList.toggle("active");
-    dropdownBtn.classList.add("pressed");
-    window.setTimeout(() => {
-        dropdownBtn.classList.remove("pressed");
-    }, 100)
-
-}, 1);
-
-function debounce(func, wait) {
-  let timeout;
-  return function(...args) {
-    clearTimeout(timeout);
-    timeout = setTimeout(() => func.apply(this, args), wait);
-  };
 }
