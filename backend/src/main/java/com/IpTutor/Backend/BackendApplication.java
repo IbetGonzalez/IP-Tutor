2241e93563162f7fc745ac9888bbeca05b9b03ce
Ibet <igonzalezviltres@ufl.edu>
2024-07-18 21:20:33 -0400

    Redirects /settings to a 401 error

--- backend/src/main/java/com/IpTutor/Backend/BackendApplication.java
+++ backend/src/main/java/com/IpTutor/Backend/BackendApplication.java
@@ -2,13 +2,16 @@ package com.IpTutor.Backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class})
 public class BackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
+
 
 }
--- backend/src/main/java/com/IpTutor/Backend/config/WebConfig.java
+++
@@ -1,2 +0,0 @@
-package com.IpTutor.Backend.config;public class WebConfig {
-}
