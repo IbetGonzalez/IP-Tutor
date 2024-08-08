53106c4b602187028c4e240f4bd94057288646d6
Ibet <igonzalezviltres@ufl.edu>
2024-08-08 12:06:23 -0400

    Integration testing set up & test for /accounts/login created

--- backend/src/main/java/com/IpTutor/Backend/controller/AccountController.java
+++ backend/src/main/java/com/IpTutor/Backend/controller/AccountController.java
@@ -126,4 +126,9 @@ public class AccountController {
                 return ResponseEntity.status(HttpStatus.OK).body("Account successfully deleted");
         }
     }
+
+    @GetMapping("/test")
+    public String test() {
+        return "Hello_world";
+    }
 }
---
+++ backend/src/test/java/com/IpTutor/Backend/AccountControllerIntegrationTests.java
@@ -0,0 +1,110 @@
+package com.IpTutor.Backend;
+
+import com.IpTutor.Backend.dto.LoginRequestDTO;
+import com.IpTutor.Backend.model.Account;
+import com.IpTutor.Backend.repository.AccountRepository;
+import com.google.gson.Gson;
+import jakarta.servlet.ServletContext;
+import org.bson.types.ObjectId;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockServletContext;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+@SpringBootTest
+class AccountControllerIntegrationTests {
+	@Autowired
+	private WebApplicationContext webApplicationContext;
+	@Autowired
+	private AccountRepository accountRepository;
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+	private MockMvc mockMvc;
+	private static String accountPassword = "Password!23";
+
+	@BeforeEach
+	public void setup() throws Exception {
+		this.mockMvc = MockMvcBuilders.webAppContextSetup(this.webApplicationContext).apply(springSecurity()).build();
+	}
+
+	private Account setUpAccount() {
+		Account account = accountRepository.findByEmail("violinsrock@gmail.com").orElse(null);
+
+		if(account == null) {
+			account = new Account();
+			account.setUsername("theMusician");
+			account.setEmail("violinsrock@gmail.com");
+			account.setPassword(passwordEncoder.encode(accountPassword));
+			account.setAccountCreation(LocalDate.now());
+			account.setId(new ObjectId());
+			account = accountRepository.save(account);
+		}
+
+		return account;
+	}
+
+	private void deleteAccount() {
+		Account account = accountRepository.findByEmail("violinsrock@gmail.com").orElse(null);
+
+		if(account != null) {
+			accountRepository.delete(account);
+		}
+	}
+
+	private LoginRequestDTO setUpLogin(String email, String password) {
+		return new LoginRequestDTO(email, password);
+	}
+
+	Gson gson = new Gson();
+
+	@Test
+	public void givenWac_whenServletContext_thenItProvidesGreetController() {
+		ServletContext servletContext = webApplicationContext.getServletContext();
+
+		assertNotNull(servletContext);
+		assertTrue(servletContext instanceof MockServletContext);
+		assertNotNull(webApplicationContext.getBean("accountController"));
+	}
+
+	@Test
+	public void login_success() throws Exception {
+		Account account = setUpAccount();
+		String json = gson.toJson(setUpLogin(account.getEmail(), accountPassword));
+		mockMvc.perform(post("/accounts/login").contentType(MediaType.APPLICATION_JSON).content(json))
+				.andExpect(status().isOk());
+		deleteAccount();
+	}
+
+	@Test
+	public void login_fail_email() throws Exception {
+		String json = gson.toJson(setUpLogin("invalidemail@ohno.org", accountPassword));
+		mockMvc.perform(post("/accounts/login").contentType(MediaType.APPLICATION_JSON).content(json))
+				.andExpect(status().isUnauthorized())
+				.andExpect(jsonPath("$.description").value("The username or password is incorrect"));
+		deleteAccount();
+	}
+
+	@Test
+	public void login_fail_password() throws Exception {
+		Account account = setUpAccount();
+		String json = gson.toJson(setUpLogin(account.getEmail(), "not" + accountPassword));
+		mockMvc.perform(post("/accounts/login").contentType(MediaType.APPLICATION_JSON).content(json))
+				.andExpect(status().isUnauthorized())
+				.andExpect(jsonPath("$.description").value("The username or password is incorrect"));
+		deleteAccount();
+	}
+
+}
