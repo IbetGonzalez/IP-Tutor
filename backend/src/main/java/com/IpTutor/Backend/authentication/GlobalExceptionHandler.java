e56d418e165b17c0c6d69d8d2933ed2fc6fd0af5
Ibet <igonzalezviltres@ufl.edu>
2024-08-12 16:30:35 -0400

    JWT token gets deleted if it is invalid

--- backend/src/main/java/com/IpTutor/Backend/authentication/GlobalExceptionHandler.java
+++ backend/src/main/java/com/IpTutor/Backend/authentication/GlobalExceptionHandler.java
@@ -1,7 +1,11 @@
 package com.IpTutor.Backend.authentication;
 
+import com.IpTutor.Backend.service.CookieService;
 import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.MalformedJwtException;
 import io.jsonwebtoken.security.SignatureException;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ProblemDetail;
 import org.springframework.security.access.AccessDeniedException;
@@ -11,10 +15,14 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
+@RequiredArgsConstructor
 public class GlobalExceptionHandler {
+
+    private final CookieService cookieService;
     @ExceptionHandler(Exception.class)
-    public ProblemDetail handleSecurityException(Exception exception) {
+    public ProblemDetail handleSecurityException(Exception exception, HttpServletResponse response) {
         ProblemDetail errorDetail = null;
+        boolean deleteCookie = false;
 
         // TODO send this stack trace to an observability tool
         exception.printStackTrace();
@@ -22,7 +30,6 @@ public class GlobalExceptionHandler {
         if (exception instanceof BadCredentialsException) {
             errorDetail = ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(401), exception.getMessage());
             errorDetail.setProperty("description", "The username or password is incorrect");
-
             return errorDetail;
         }
 
@@ -39,16 +46,29 @@ public class GlobalExceptionHandler {
         if (exception instanceof SignatureException) {
             errorDetail = ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(403), exception.getMessage());
             errorDetail.setProperty("description", "The JWT signature is invalid");
+            deleteCookie = true;
         }
 
         if (exception instanceof ExpiredJwtException) {
             errorDetail = ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(403), exception.getMessage());
             errorDetail.setProperty("description", "The JWT token has expired");
+            deleteCookie = true;
+        }
+
+        if(exception instanceof MalformedJwtException) {
+            errorDetail = ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(403), exception.getMessage());
+            errorDetail.setProperty("description", " JWT strings must contain exactly 2 period characters.");
+            deleteCookie = true;
         }
 
         if (errorDetail == null) {
             errorDetail = ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(500), exception.getMessage());
             errorDetail.setProperty("description", "Unknown internal server error.");
+            deleteCookie = true;
+        }
+
+        if(deleteCookie) {
+            response.addCookie(cookieService.deleteJwtToken().jwt_token());
         }
 
         return errorDetail;
